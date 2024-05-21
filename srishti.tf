39e576f99286dd6eba11356fe6e5ef43318db9ea
Srishti Agarwal <s3.agarwal@partner.samsung.com>
2024-05-21 22:03:00 +0530

    Update srishti.tf

--- srishti.tf
+++ srishti.tf
@@ -6,14 +6,7 @@ resource "aws_iam_user" "srishti" {
   name = "srishti"  # Replace with the desired username for the new IAM user
 }
 
-resource "aws_iam_access_key" "new_user_access_key" {
-  user = aws_iam_user.new_user.name
-
-  # Uncomment the following line if you want to manage the access key lifecycle with Terraform
-  # lifecycle { create_before_destroy = true }
-}
-
 resource "aws_iam_user_policy_attachment" "new_user_policy_attachment" {
-  user       = aws_iam_user.new_user.name
+  user       = aws_iam_user.srishti.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"  # Example: Attaching AdministratorAccess policy, adjust as needed
 }
