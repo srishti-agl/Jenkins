c32717e4dc0da7b599f601b0a5340145cebfc002
Srishti Agarwal <s3.agarwal@partner.samsung.com>
2024-05-22 15:46:59 +0530

    Update srishti.tf

--- srishti.tf
+++ srishti.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "us-east-1"  # Change this to your desired AWS region
+  access_key = AKIAXYKJR7RK6BIHN3CE
+  secret_key = AOVLlQLtx0geO+xfng3ECN0+OP5db8uG1XCbfkdt
 }
 
 resource "aws_iam_user" "srishti" {
